9e41836101c4ff216c73a3471ef918ce4937278e
Captain Developer <muhammadcaptain303@gmail.com>
2026-06-27 02:23:50 +0500

    thired commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://your-frontend-domain.vercel.app'],
+    origin: ['http://localhost:5173', 'https://your-frontend-domain.vercel.app', 'https://farahcanvas.netlify.app/'],
     credentials: true
 }));
 app.use(morgan('dev'));
